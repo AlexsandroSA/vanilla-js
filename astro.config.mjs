afafd1fce3ce9d9de6e6bb76b6a4750cdc3f7ea6
Alexsandro Silva Alves <alexsando.s.alves@gmail.com>
2025-03-02 08:05:39 -0300

    chore: setup google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,21 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://www.vanilla-js.com.br/",
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
 	vite: {
 		plugins: [tailwindcss()],
 	},

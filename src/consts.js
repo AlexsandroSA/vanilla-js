accf6d22aa65e01022e7a1c585cc109ef0a25a6d
Alexsandro Silva Alves <alexsando.s.alves@gmail.com>
2025-03-06 20:53:02 -0300

    refactor: add constant language

--- src/consts.js
+++ src/consts.js
@@ -2,4 +2,5 @@ export const SITE = {
 	TITLE: "VanillaJS",
 	DESCRIPTION:
 		"Aventure-se no mundo do VanillaJS e descubra a verdadeira essência da linguagem que move a web.",
+	LANGUAGE: 'pt-BR'
 };

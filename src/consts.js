2224764cf76c62b9c2cd8791a81d5a5003c4159a
Alexsandro Silva Alves <alexsando.s.alves@gmail.com>
2025-12-15 21:02:41 -0300

    feat: add author info and improve navigation

--- src/consts.js
+++ src/consts.js
@@ -4,3 +4,10 @@ export const SITE = {
 		"Aventure-se no mundo do VanillaJS e descubra a verdadeira essência da linguagem que move a web.",
 	LANGUAGE: "pt-BR",
 };
+
+export const AUTHOR = {
+	NAME: "Alexsandro Silva Alves",
+	BIO: "Desenvolvedor apaixonado por JavaScript puro e tecnologias web.",
+	GITHUB: "https://github.com/AlexsandroSA",
+	WEBSITE: "https://alexsandro-sa.com.br",
+};
